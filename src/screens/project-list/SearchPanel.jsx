120c05bd5734f16341229e77e1e0c26c6288e98f
Savannah <71588465+SavanCode@users.noreply.github.com>
2021-06-29 11:20:11 +0800

    add new

---
+++ src/screens/project-list/SearchPanel.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export const SearchPanel = (param, setParam, users) => {
+  console.log(users)
+  return (
+    <form action="">
+      <div>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(event) =>
+            setParam({
+              ...param,
+              name: event.target.value,
+            })
+          }
+        />
+        <select
+          name=""
+          id=""
+          value={param.personId}
+          onChange={(event) =>
+            setParam({
+              ...param,
+              personId: event.target.value,
+            })
+          }
+        >
+          <option value={""}>负责人</option>
+          {
+             //users.map(user =><option value={user.id}>{user.name}</option>)
+          }
+        </select>
+      </div>
+    </form>
+  );
+};
---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,48 @@
+import React from "react"
+import { useEffect,useState } from "react"
+import { SearchPanel } from "./SearchPanel"
+import { List } from "./List" 
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const ProjectListScreen=()=>{
+    const [users,setUsers] = useState([])
+    const [list,setList] = useState([])
+    const [param ,setParam] = useState({
+        name:'',
+        personId:''
+    })
+    //请求数据
+    useEffect(()=>{   
+        fetch(`${apiUrl}/projects`).then(async response=>{
+            if(response.ok){
+                setList(await response.json())
+            }
+        })
+    },[param])
+    
+    useEffect(()=>{  
+        fetch(`${apiUrl}/users`).then(async response=>{
+            if(response.ok){
+                console.log(users)
+                setUsers(await response.json())
+            }
+        })
+    },[])
+
+    // useEffect(()=>{  
+    //     fetch('http://localhost:3001/users')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //     console.log('Success:', data);
+    //     })
+    //     .catch((error) => {
+    //     console.error('Error:', error);
+    //     });
+    // },[])
+ 
+    return <div>
+        <SearchPanel param={param} setParam={setParam} users={users}/>
+        <List list={list} users={users}/>
+    </div>
+}
